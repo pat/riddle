8593b5fdd0e0654a64ea84c3785dc5b3c612a597
Pat Allan <pat@freelancing-gods.com>
2007-11-04 10:36:48 +0000

    Adding basic structure to run sphinx for specs
    git-svn-id: http://rails-oceania.googlecode.com/svn/patallan/riddle/trunk@466 44c325e4-a42c-0410-87d1-89eb90bb738d

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,19 @@
 require 'riddle'
+require 'spec/sphinx_helper'
 
 Spec::Runner.configure do |config|
-  #
+  sphinx = SphinxHelper.new
+  sphinx.setup_mysql
+  sphinx.generate_configuration
+  sphinx.index
+  
+  config.before :all do
+    sphinx.start
+  end
+  
+  config.after :all do
+    sphinx.stop
+  end
 end
 
 def query_contents(key)
