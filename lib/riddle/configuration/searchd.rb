0486ed7f9de95c23efc007b16764319fce8f0996
Pat Allan <pat@freelancing-gods.com>
2009-11-09 14:58:38 +1100

    Removing generated spec data files - no need to store them in git. Also, fixing config side of things for multi-version support.

--- lib/riddle/configuration/searchd.rb
+++ lib/riddle/configuration/searchd.rb
@@ -20,26 +20,7 @@ module Riddle
       end
       
       def valid?
-        set_listen
-        clear_deprecated
-        
-        !( @listen.nil? || @pid_file.nil? )
-      end
-      
-      private
-      
-      def set_listen
-        return unless @listen.nil?
-        
-        @listen = @port.to_s if @port && @address.nil?
-        @listen = "#{@address}:#{@port}" if @address && @port
-      end
-      
-      def clear_deprecated
-        return if @listen.nil?
-        
-        @address  = nil
-        @port     = nil
+        !( @port.nil? || @pid_file.nil? )
       end
     end
   end
--- spec/unit/configuration/searchd_spec.rb
+++ spec/unit/configuration/searchd_spec.rb
@@ -1,22 +1,38 @@
 require 'spec/spec_helper'
 
 describe Riddle::Configuration::Searchd do
-  it "should be invalid without a listen or pid_file" do
-    searchd = Riddle::Configuration::Searchd.new
-    searchd.should_not be_valid
+  if SphinxVersion == '0.9.9'
+    it "should be invalid without a listen or pid_file" do
+      searchd = Riddle::Configuration::Searchd.new
+      searchd.should_not be_valid
     
-    searchd.port = 3312
-    searchd.should_not be_valid
+      searchd.port = 3312
+      searchd.should_not be_valid
     
-    searchd.pid_file = "file.pid"
-    searchd.should be_valid
+      searchd.pid_file = "file.pid"
+      searchd.should be_valid
     
-    searchd.port    = nil
-    searchd.listen  = nil
-    searchd.should_not be_valid
+      searchd.port    = nil
+      searchd.listen  = nil
+      searchd.should_not be_valid
     
-    searchd.listen = "localhost:3312"
-    searchd.should be_valid
+      searchd.listen = "localhost:3312"
+      searchd.should be_valid
+    end
+  else
+    it "should be invalid without a port or pid_file" do
+      searchd = Riddle::Configuration::Searchd.new
+      searchd.should_not be_valid
+
+      searchd.port = 3312
+      searchd.should_not be_valid
+
+      searchd.pid_file = "file.pid"
+      searchd.should be_valid
+
+      searchd.port = nil
+      searchd.should_not be_valid
+    end
   end
   
   it "should raise a ConfigurationError if rendering but not valid" do
@@ -44,12 +60,22 @@ describe Riddle::Configuration::Searchd do
     searchd.port      = 3312
     searchd.pid_file  = "file.pid"
     
-    searchd.render.should == <<-SEARCHD
+    if SphinxVersion == '0.9.9'
+      searchd.render.should == <<-SEARCHD
 searchd
 {
   listen = 3312
   pid_file = file.pid
 }
-    SEARCHD
+      SEARCHD
+    else
+      searchd.render.should == <<-SEARCHD
+searchd
+{
+  port = 3312
+  pid_file = file.pid
+}
+      SEARCHD
+    end
   end
 end
