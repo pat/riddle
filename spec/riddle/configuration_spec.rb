2f8f64c6b4e20c2fad4aa685da1241b473cb1973
Pat Allan <pat@freelancing-gods.com>
2009-12-08 10:47:21 +1100

    Removing version check from Configuration#initialize

--- spec/riddle/configuration_spec.rb
+++ spec/riddle/configuration_spec.rb
@@ -1,11 +1,5 @@
 require 'spec/spec_helper'
 
 describe Riddle::Configuration do
-  describe '#initialize' do
-    it "should check the loaded Sphinx version" do
-      Riddle.should_receive(:version_warning)
-      
-      Riddle::Configuration.new
-    end
-  end
+  #
 end
