0dcaad1a57e683ef6dc6f5f2bb9bb158e1b7d54d
Pat Allan <pat@freelancing-gods.com>
2011-01-24 22:37:06 +1100

    Updating home page to point to the more recent (yet still lacking) documentation.

--- tasks/distribution.rb
+++ tasks/distribution.rb
@@ -8,7 +8,7 @@ Jeweler::Tasks.new do |gem|
   gem.summary     = 'An API for Sphinx, written in and for Ruby.'
   gem.description = 'A Ruby API and configuration helper for the Sphinx search service.'
   gem.email       = "pat@freelancing-gods.com"
-  gem.homepage    = "http://riddle.freelancing-gods.com"
+  gem.homepage    = "http://freelancing-god.github.com/riddle/"
   gem.authors     = ["Pat Allan"]
   
   gem.add_development_dependency "rspec",    ">= 1.2.9"
