36ac02c560dc0fb025fb8f0e0753e09f9bc2f78f
Pat Allan <pat@freelancing-gods.com>
2011-08-21 14:07:30 +1000

    Use searchd to manage stopping the daemon.

--- spec/support/sphinx.rb
+++ spec/support/sphinx.rb
@@ -8,7 +8,7 @@ end
 
 class Sphinx
   attr_accessor :host, :username, :password
-  
+
   def initialize
     self.host     = 'localhost'
     self.username = 'root'
@@ -21,10 +21,10 @@ class Sphinx
       self.password = config['password']
     end
   end
-  
+
   def setup_mysql
     return setup_mysql_on_jruby if RUBY_PLATFORM == 'java'
-    
+
     client = Mysql2::Client.new(
       :host     => host,
       :username => username,
@@ -37,7 +37,7 @@ class Sphinx
     end
 
     client.query 'USE riddle'
-    
+
     structure = File.open('spec/fixtures/sql/structure.sql') { |f| f.read }
     structure.split(/;/).each { |sql| client.query sql }
     client.query <<-SQL
@@ -49,21 +49,21 @@ class Sphinx
 
     client.close
   end
-  
+
   def setup_mysql_on_jruby
     address = "jdbc:mysql://#{host}"
     client = java.sql.DriverManager.getConnection(address, username, password)
-    
+
     set       = client.createStatement.executeQuery('SHOW DATABASES')
     databases = []
     databases << set.getString(1) while set.next
-    
+
     unless databases.include?('riddle')
       client.createStatement.execute 'CREATE DATABASE riddle'
     end
 
     client.createStatement.execute 'USE riddle'
-    
+
     structure = File.open('spec/fixtures/sql/structure.sql') { |f| f.read }
     structure.split(/;/).each { |sql| client.createStatement.execute sql }
     client.createStatement.execute <<-SQL
@@ -73,20 +73,20 @@ class Sphinx
       postcode, email, birthday)
     SQL
   end
-  
+
   def generate_configuration
     template = File.open('spec/fixtures/sphinx/configuration.erb') { |f| f.read }
     File.open('spec/fixtures/sphinx/spec.conf', 'w') { |f|
       f.puts ERB.new(template).result(binding)
     }
   end
-  
+
   def index
     cmd = "#{bin_path}indexer --config #{fixtures_path}/sphinx/spec.conf --all"
     cmd << ' --rotate' if running?
     `#{cmd}`
   end
-  
+
   def start
     return if running?
 
@@ -98,14 +98,17 @@ class Sphinx
       puts 'Failed to start searchd daemon. Check fixtures/sphinx/searchd.log.'
     end
   end
-  
+
   def stop
     return unless running?
-    `kill #{pid}`
+
+    stop_flag = '--stopwait'
+    stop_flag = '--stop' if Riddle.loaded_version.to_i < 1
+    `#{bin_path}searchd --config #{fixtures_path}/sphinx/spec.conf #{stop_flag}`
   end
-  
+
   private
-  
+
   def pid
     if File.exists?("#{fixtures_path}/sphinx/searchd.pid")
       `cat #{fixtures_path}/sphinx/searchd.pid`[/\d+/]
@@ -117,11 +120,11 @@ class Sphinx
   def running?
     pid && `ps #{pid} | wc -l`.to_i > 1
   end
-  
+
   def fixtures_path
     File.expand_path File.join(File.dirname(__FILE__), '..', 'fixtures')
   end
-  
+
   def bin_path
     @bin_path ||= begin
       path = (ENV['SPHINX_BIN'] || '').dup
