22106fc348a68aa66a712649afdb0811cb79f00d
Pat Allan <pat@freelancing-gods.com>
2011-11-03 18:07:57 +0200

    White space pedantry.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'bundler'
 $:.unshift File.dirname(__FILE__) + '/../lib'
 $:.unshift File.dirname(__FILE__) + '/..'
 
-Dir['spec/support/**/*.rb'].each {|f| require f}
+Dir['spec/support/**/*.rb'].each { |f| require f }
 
 Bundler.require :default, :development
 
@@ -12,18 +12,18 @@ require 'riddle'
 
 RSpec.configure do |config|
   config.include BinaryFixtures
-  
+
   sphinx = Sphinx.new
   sphinx.setup_mysql
   sphinx.generate_configuration
   sphinx.index
-  
+
   BinaryFixtures.build_fixtures Riddle.loaded_version
-  
+
   config.before :all do |group|
     sphinx.start if group.class.metadata[:live]
   end
-  
+
   config.after :all do |group|
     sphinx.stop if group.class.metadata[:live]
   end
