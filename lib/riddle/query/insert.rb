869fe97d166ede96ed39ed923af709fc5d42b0ee
Pat Allan <pat@freelancing-gods.com>
2013-06-22 16:46:36 +1000

    Arrays in INSERT/REPLACE get wrapped in parentheses.
    With values separated by commas. Required for MVA values in real-time indices.

--- lib/riddle/query/insert.rb
+++ lib/riddle/query/insert.rb
@@ -1,32 +1,32 @@
 class Riddle::Query::Insert
   attr_reader :columns, :values
-  
+
   def initialize(index, columns = [], values = [])
     @index   = index
     @columns = columns
     @values  = values.first.is_a?(Array) ? values : [values]
     @replace = false
   end
-  
+
   def replace!
     @replace = true
     self
   end
-  
+
   def to_sql
     "#{command} INTO #{@index} (#{columns_to_s}) VALUES (#{values_to_s})"
   end
-  
+
   private
-  
+
   def command
     @replace ? 'REPLACE' : 'INSERT'
   end
-  
+
   def columns_to_s
     columns.join(', ')
   end
-        
+
   def values_to_s
     values.collect { |value_set|
       value_set.collect { |value|
@@ -34,7 +34,7 @@ class Riddle::Query::Insert
       }.join(', ')
     }.join('), (')
   end
-  
+
   def translated_value(value)
     case value
     when String
@@ -43,6 +43,8 @@ class Riddle::Query::Insert
       value ? 1 : 0
     when Time
       value.to_i
+    when Array
+      "(#{value.join(',')})"
     else
       value
     end
