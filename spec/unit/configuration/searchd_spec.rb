0dfe38063c9650862c1adf14257d38d6183aea35
Pat Allan <pat@freelancing-gods.com>
2014-03-23 12:08:13 +1100

    Hold onto address and port when crafting listen, but don't render them.

--- spec/unit/configuration/searchd_spec.rb
+++ spec/unit/configuration/searchd_spec.rb
@@ -5,17 +5,17 @@ describe Riddle::Configuration::Searchd do
     it "should be invalid without a listen or pid_file" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.should_not be_valid
-      
+
       searchd.port = 3312
       searchd.should_not be_valid
-      
+
       searchd.pid_file = "file.pid"
       searchd.should be_valid
-      
+
       searchd.port    = nil
       searchd.listen  = nil
       searchd.should_not be_valid
-      
+
       searchd.listen = "localhost:3312"
       searchd.should be_valid
     end
@@ -34,13 +34,13 @@ describe Riddle::Configuration::Searchd do
       searchd.should_not be_valid
     end
   end
-  
+
   it "should raise a ConfigurationError if rendering but not valid" do
     searchd = Riddle::Configuration::Searchd.new
     searchd.should_not be_valid
     lambda { searchd.render }.should raise_error(Riddle::Configuration::ConfigurationError)
   end
-  
+
   it "should support Sphinx's searchd settings" do
     settings = %w( listen address port log query_log read_timeout
       client_timeout max_children pid_file max_matches seamless_rotate
@@ -48,7 +48,7 @@ describe Riddle::Configuration::Searchd do
       max_packet_size mva_updates_pool crash_log_path max_filters
       max_filter_values )
     searchd = Riddle::Configuration::Searchd.new
-    
+
     settings.each do |setting|
       searchd.should respond_to(setting.to_sym)
       searchd.should respond_to("#{setting}=".to_sym)
@@ -109,7 +109,7 @@ searchd
     searchd.port       = 3312
     searchd.pid_file   = 'file.pid'
     searchd.client_key = 'secret'
-    
+
     if Riddle.loaded_version.to_f >= 0.9
       searchd.render.should == <<-SEARCHD
 searchd
@@ -130,13 +130,13 @@ searchd
       SEARCHD
     end
   end
-  
+
   if Riddle.loaded_version.to_f >= 0.9
     it "should render with mysql41 on the default port if true" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.mysql41  = true
       searchd.pid_file = 'file.pid'
-      
+
       searchd.render.should == <<-SEARCHD
 searchd
 {
@@ -145,12 +145,12 @@ searchd
 }
       SEARCHD
     end
-    
+
     it "should render with mysql41 on a given port" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.mysql41  = 9307
       searchd.pid_file = 'file.pid'
-      
+
       searchd.render.should == <<-SEARCHD
 searchd
 {
@@ -159,13 +159,13 @@ searchd
 }
       SEARCHD
     end
-    
+
     it "allows for both normal and mysql41 connections" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.mysql41  = true
       searchd.port     = 9312
       searchd.pid_file = 'file.pid'
-      
+
       searchd.render.should == <<-SEARCHD
 searchd
 {
@@ -175,13 +175,13 @@ searchd
 }
       SEARCHD
     end
-    
+
     it "uses given address for mysql41 connections" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.mysql41  = true
       searchd.address  = '127.0.0.1'
       searchd.pid_file = 'file.pid'
-      
+
       searchd.render.should == <<-SEARCHD
 searchd
 {
@@ -190,14 +190,14 @@ searchd
 }
       SEARCHD
     end
-    
+
     it "applies the given address to both normal and mysql41 connections" do
       searchd = Riddle::Configuration::Searchd.new
       searchd.mysql41  = true
       searchd.port     = 9312
       searchd.address  = 'sphinx.server.local'
       searchd.pid_file = 'file.pid'
-      
+
       searchd.render.should == <<-SEARCHD
 searchd
 {
@@ -206,6 +206,23 @@ searchd
   pid_file = file.pid
 }
       SEARCHD
+    end
+
+    it 'maintains the address and port settings without rendering them' do
+      searchd          = Riddle::Configuration::Searchd.new
+      searchd.port     = 9312
+      searchd.address  = 'sphinx.server.local'
+      searchd.pid_file = 'file.pid'
+
+      searchd.render.should == <<-SEARCHD
+searchd
+{
+  listen = sphinx.server.local:9312
+  pid_file = file.pid
+}
+      SEARCHD
+      searchd.address.should == 'sphinx.server.local'
+      searchd.port.should == 9312
     end
   end
 end
