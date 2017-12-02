220d8a2aead13e6b8de6c3240e1d4c5d245a5f63
Pat Allan <pat@freelancing-gods.com>
2017-12-02 19:54:00 +1100

    Don’t presume Mysql2::Client exists if Mysql2 does
    It seems Mysql2 is sometimes defined when using JRuby. I’m not sure where or why, but it doesn’t hurt to be a bit more precise.

--- lib/riddle/query.rb
+++ lib/riddle/query.rb
@@ -4,6 +4,7 @@ module Riddle::Query
   ESCAPE_CHARACTERS = /[\(\)\|\-!@~\/"\/\^\$\\><&=\?]/
   # http://sphinxsearch.com/docs/current/extended-syntax.html
   ESCAPE_WORDS = /\b(?:MAYBE|NEAR|PARAGRAPH|SENTENCE|ZONE|ZONESPAN)\b/
+  MYSQL2_ESCAPE = defined?(Mysql2) && defined?(Mysql::Client)
 
   def self.connection(address = '127.0.0.1', port = 9312)
     require 'mysql2'
@@ -114,7 +115,7 @@ module Riddle::Query
   end
 
   def self.sql_escape(string)
-    return Mysql2::Client.escape(string) if defined?(Mysql2)
+    return Mysql2::Client.escape(string) if MYSQL2_ESCAPE
 
     string.gsub(/['"\\]/) { |character| "\\#{character}" }
   end
